74a95652a2dcf6aa1a9afb8836f457334c1069a6
Johan17 <danilobaronmartinez17@gmail.com>
2025-08-06 23:26:23 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,349 @@
+const productos = [
+    //Desayunos
+        {
+        "id": "desayuno-01",
+        "titulo": "Desayuno 01",
+        "imagen": "./img/desayunos/01.jpg",
+        "categoria": {
+            "nombre": "Desayunos",
+            "id": "desayunos"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "desayuno-02",
+        "titulo": "Desayuno 02",
+        "imagen": "./img/desayunos/02.jpg",
+        "categoria": {
+            "nombre": "Desayunos",
+            "id": "desayunos"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "desayuno-03",
+        "titulo": "Desayuno 03",
+        "imagen": "./img/desayunos/03.jpg",
+        "categoria": {
+            "nombre": "Desayunos",
+            "id": "desayunos"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "desayuno-04",
+        "titulo": "Desayuno 04",
+        "imagen": "./img/desayunos/04.jpg",
+        "categoria": {
+            "nombre": "Desayunos",
+            "id": "desayunos"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "desayuno-05",
+        "titulo": "Desayuno 05",
+        "imagen": "./img/desayunos/05.jpg",
+        "categoria": {
+            "nombre": "Desayunos",
+            "id": "desayunos"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "desayuno-06",
+        "titulo": "Desayuno 06",
+        "imagen": "./img/desayunos/06.jpg",
+        "categoria": {
+            "nombre": "Desayunos",
+            "id": "desayunos"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "desayuno-07",
+        "titulo": "Desayuno 07",
+        "imagen": "./img/desayunos/07.jpg",
+        "categoria": {
+            "nombre": "Desayunos",
+            "id": "desayunos"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "desayuno-08",
+        "titulo": "Desayuno 08",
+        "imagen": "./img/desayunos/08.jpg",
+        "categoria": {
+            "nombre": "Desayunos",
+            "id": "desayunos"
+        },
+        "precio": 1000
+        },
+    //Almuerzos
+        {
+        "id": "almuerzo-01",
+        "titulo": "Almuerzo 01",
+        "imagen": "./img/almuerzos/01.jpg",
+        "categoria": {
+            "nombre": "Almuerzos",
+            "id": "almuerzos"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "almuerzo-02",
+        "titulo": "Almuerzo 02",
+        "imagen": "./img/almuerzos/02.jpg",
+        "categoria": {
+            "nombre": "Almuerzos",
+            "id": "almuerzos"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "almuerzo-03",
+        "titulo": "Almuerzo 03",
+        "imagen": "./img/almuerzos/03.jpg",
+        "categoria": {
+            "nombre": "Almuerzos",
+            "id": "almuerzos"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "almuerzo-04",
+        "titulo": "Almuerzo 04",
+        "imagen": "./img/almuerzos/04.jpg",
+        "categoria": {
+            "nombre": "Almuerzos",
+            "id": "almuerzos"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "almuerzo-05",
+        "titulo": "Almuerzo 05",
+        "imagen": "./img/almuerzos/05.jpg",
+        "categoria": {
+            "nombre": "Almuerzos",
+            "id": "almuerzos"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "almuerzo-06",
+        "titulo": "Almuerzo 06",
+        "imagen": "./img/almuerzos/06.jpg",
+        "categoria": {
+            "nombre": "Almuerzos",
+            "id": "almuerzos"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "almuerzo-07",
+        "titulo": "Almuerzo 07",
+        "imagen": "./img/almuerzos/07.jpg",
+        "categoria": {
+            "nombre": "Almuerzos",
+            "id": "almuerzos"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "almuerzo-08",
+        "titulo": "Almuerzo 08",
+        "imagen": "./img/almuerzos/08.jpg",
+        "categoria": {
+            "nombre": "Almuerzos",
+            "id": "almuerzos"
+        },
+        "precio": 1000
+        },
+    //Cenas
+        {
+        "id": "cena-01",
+        "titulo": "Cena 01",
+        "imagen": "./img/cenas/01.jpg",
+        "categoria": {
+            "nombre": "Cenas",
+            "id": "cenas"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "cena-02",
+        "titulo": "Cena 02",
+        "imagen": "./img/cenas/02.jpg",
+        "categoria": {
+            "nombre": "Cenas",
+            "id": "cenas"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "cena-03",
+        "titulo": "Cena 03",
+        "imagen": "./img/cenas/03.jpg",
+        "categoria": {
+            "nombre": "Cenas",
+            "id": "cenas"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "cena-04",
+        "titulo": "Cena 04",
+        "imagen": "./img/cenas/04.jpg",
+        "categoria": {
+            "nombre": "Cenas",
+            "id": "cenas"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "cena-05",
+        "titulo": "Cena 05",
+        "imagen": "./img/cenas/05.jpg",
+        "categoria": {
+            "nombre": "Cenas",
+            "id": "cenas"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "cena-06",
+        "titulo": "Cena 06",
+        "imagen": "./img/cenas/06.jpg",
+        "categoria": {
+            "nombre": "Cenas",
+            "id": "cenas"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "cena-07",
+        "titulo": "Cena 07",
+        "imagen": "./img/cenas/07.jpg",
+        "categoria": {
+            "nombre": "Cenas",
+            "id": "cenas"
+        },
+        "precio": 1000
+        },
+        {
+        "id": "cena-08",
+        "titulo": "Cena 08",
+        "imagen": "./img/cenas/08.jpg",
+        "categoria": {
+            "nombre": "Cenas",
+            "id": "cenas"
+        },
+        "precio": 1000
+        }
+]
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".porducto-agregar");
+const numerito = document.querySelector("#numerito")
+
+function cargarProductos(productosElegidos){
+    contenedorProductos.innerHTML = "";
+
+    productosElegidos.forEach(producto =>{
+
+        
+
+        const div = document.createElement("div");
+        div.classList.add("producto")
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `;
+
+        contenedorProductos.append(div);
+
+    })
+
+    actualizarBotonesAgregar();
+
+}
+
+cargarProductos(productos);
+
+botonesCategorias.forEach(boton => {
+
+    boton.addEventListener("click", (e) =>{
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");;
+
+        if (e.currentTarget.id != "todos"){
+            const productoCatergoria = productos.find(producto => producto.categoria.id ===e.currentTarget.id);
+            tituloPrincipal.innerHTML = productoCatergoria.categoria.nombre; 
+
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        }
+
+
+        else{
+            tituloPrincipal.innerHTML = "Menu Completo";
+            cargarProductos(productos);
+        }
+    })
+
+})
+
+function actualizarBotonesAgregar(){
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if(productosEnCarritoLS){
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+}else{
+    productosEnCarrito = [];
+}
+
+
+function agregarAlCarrito(e){
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton))
+        {
+            const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+            productosEnCarrito[index].cantidad++;
+        }
+    else
+        {
+            productoAgregado.cantidad = 1;
+            productosEnCarrito.push(productoAgregado);
+        }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumerito(){
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
